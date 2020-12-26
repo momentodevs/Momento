4c2cbcd6e6f352e05e2e038f1c9e2952badae659
TimmyTime <53761355+timmy-time@users.noreply.github.com>
2020-12-27 05:34:56 +1000

    Updated index.py

--- index.py
+++ index.py
@@ -13,30 +13,6 @@ bot = commands.AutoShardedBot(command_prefix=commands.when_mentioned_or("m?"), d
 with open("config.toml") as f:
     bot.config = toml.load(f)
 
-@client.command()
-@commands.has_permissions(administrator=True)
-async def load(context, extension):
-    bot.load_extension(f'cogs.{extension}')
-    print("loaded "f'cogs.{extension}' + "...")
-
-@client.command()
-@commands.has_permissions(administrator=True)
-async def unload(context, extension):
-    bot.unload_extension(f'cogs.{extension}')
-    print("unloaded "f'cogs.{extension}' + "...")
-
-
-@client.command()
-@commands.has_permissions(administrator=True)
-async def reload(context, extension):
-    bot.reload_extension(f'cogs.{extension}')
-    print("reloaded "f'cogs.{extension}' + "...")
-
-for filename in os.listdir('./cogs'):
-    if filename.endswith('.py'):
-        bot.load_extension(f'cogs.{filename[:-3]}')
-
-
 nav = Navigation("\U000025c0\U0000fe0f", "\U000025b6\U0000fe0f")
 color = discord.Color.blue()
 
