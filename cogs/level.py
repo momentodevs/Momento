95a9087d0478273c33f95b68996e24cfccd9a41d
TimPidashev <pidashev.tim@gmail.com>
2021-01-14 08:51:14 -0800

    really minor level update

--- cogs/level.py
+++ cogs/level.py
@@ -1,15 +1,22 @@
 import discord
 from discord.ext import commands
-import psycopg2
-import sys, os
-import numpy as np
-import pandas as pd
-import example_psql as creds
-import pandas.io.sql as psql
+import asyncio
+import asyncpg
+import time
+import os
 
 class Level(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+
+    @commands.Cog.listener()
+    async def on_ready(self):
+        credentials = {"user": "USERNAME", "password": "PASSWORD", "database": "DATABSE", "host": "127.0.0.1"}
+         try:
+             await bot.start(config.token)
+         except KeyboardInterrupt:
+             await db.close()
+             await bot.logout()
 
 
 def setup(client):
