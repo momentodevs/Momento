f8853f3149bd540e7c97adc6b584a1c9f4a330b3
Brayden Kennedy-Wilcox <67422779+Mamoth112@users.noreply.github.com>
2021-04-23 23:21:04 +0800

    Added Mamoths PostGres DB Handler v1

--- db/db.py
+++ db/db.py
@@ -1,74 +1,64 @@
-from os.path import isfile
-from sqlite3 import connect
-
-from apscheduler.triggers.cron import CronTrigger
-
-DB_PATH = "./data/database.db"
-BUILD_PATH = "./data/build.sql"
-
-cxn = connect(DB_PATH, check_same_thread=False)
-cur = cxn.cursor()
+import psycopg2
+from datetime import datetime
+import atexit
+import random
 
 
-def with_commit(func):
-    def inner(*args, **kwargs):
-        func(*args, **kwargs)
-        commit()
-
-    return inner
-
-
-@with_commit
-def build():
-    if isfile(BUILD_PATH):
-        scriptexec(BUILD_PATH)
+def connect_db():
+    con = psycopg2.connect(host="",
+                            database="", user="",
+                            password="", port="")
+    return con
 
 
-def commit():
-    cxn.commit()
+conn = connect_db()
+print(conn)
 
 
-def autosave(sched):
-    sched.add_job(commit, CronTrigger(second=0))
+def create_users_table(conn):
+    cur = conn.cursor()
+    cur.execute(
+        "CREATE TABLE users ( id varchar PRIMARY KEY, guild_id varchar, invites integer, xp integer, level integer, warns integer, mutes integer, messages_sent integer, join_date timestamp, songs_played integer);")
+    cur.close()
 
 
-def close():
-    cxn.close()
+def insert_test_user_data(conn):
+    cur = conn.cursor()
+    cur.execute(
+        'INSERT INTO users (id, guild_id, invites, xp, level, warns, mutes, messages_sent, join_date, songs_played) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s);', (616582088845688843, 734397485346455572, 0, 0, 0, 0, 0, 0, "2021-4-22", 0))
+    conn.commit()
+    cur.close()
 
 
-def field(command, *values):
-    cur.execute(command, tuple(values))
-
-    if (fetch := cur.fetchone()) is not None:
-        return fetch[0]
+def disconnect_db(conn):
+    conn.close()
 
 
-def record(command, *values):
-    cur.execute(command, tuple(values))
-
-    return cur.fetchone()
-
-
-def records(command, *values):
-    cur.execute(command, tuple(values))
-
-    return cur.fetchall()
+def runCommand(*args):
+    commandToRun = args[0]
+    commands= ['userMessagesIncrement']
+    for i in commands:
+        if commandToRun == i:
+            globals()[commandToRun](conn, args)
 
 
-def column(command, *values):
-    cur.execute(command, tuple(values))
+def userMessagesIncrement(conn, args):
+    userId = args[1]
+    guildId = args[2]
+    cur = conn.cursor()
+    cur.execute("INSERT INTO users (id, guild_id) VALUES (%s, %s) ON CONFLICT DO NOTHING;", (str(userId), str(guildId)))
+    conn.commit()
+    cur.close()
+    cur = conn.cursor()
+    cur.execute("UPDATE users SET messages_sent = messages_sent + 1 WHERE (id = %s::varchar AND guild_id = %s::varchar);", (userId, guildId))
+    conn.commit()
+    cur.close()
 
-    return [item[0] for item in cur.fetchall()]
+
+def getLevels(conn, args):
+    userId = args[1]
+    guild_id= args[2]
+    cur = conn.cursor()
 
 
-def execute(command, *values):
-    cur.execute(command, tuple(values))
-
-
-def multiexec(command, valueset):
-    cur.executemany(command, valueset)
-
-
-def scriptexec(path):
-    with open(path, "r", encoding="utf-8") as script:
-        cur.executescript(script.read())
+atexit.register(disconnect_db, conn=conn)
