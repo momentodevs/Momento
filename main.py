57394e6f08cee97bb54392cb11f44692415c5e00
Mamoth112 <bkennedy-wilcox@student.cewa.edu.au>
2022-03-08 18:45:57 +0800

    completed main.py and added config.json

--- main.py
+++ main.py
@@ -177,4 +177,32 @@ async def on_command_error(context: Context, error) -> None:
     :param context: The normal command that failed executing.
     :param error: The error that has been faced.
     """
-    if isinstance()
+    if isinstance(error, commands.CommandOnCooldown):
+        minutes, seconds = divmod(error.retry_after, 60)
+        hours, minutes = divmod(minutes, 60)
+        hours = hours % 24
+        embed = nextcord.Embed(
+            title="Hey, please slow down!",
+            description=f"You can use this command again in {f'{round(hours)} hours' if round(hours) > 0 else ''} {f'{round(minutes)} minutes' if round(minutes) > 0 else ''} {f'{round(seconds)} seconds' if round(seconds) > 0 else ''}.",
+            color=0xE02B2B
+        )
+        await context.send(embed=embed)
+    elif isinstance(error, commands.MissingPermissions):
+        embed = nextcord.Embed(
+            title="Error!",
+            description="You are missing the permission(s) `" + ", ".join(
+                error.missing_permissions) + "` to execute this command!",
+            color=0xE02B2B
+        )
+        await context.send(embed=embed)
+    elif isinstance(error, commands.MissingRequiredArgument):
+        embed = nextcord.Embed(
+            title="Error!",
+            description=str(error).capitalize(),
+            # We need to capitalize because the command arguments have no capital letter in the code.
+            color=0xE02B2B
+        )
+        await context.send(embed=embed)
+    raise error
+
+bot.run(config['token'])
